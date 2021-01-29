89c43d6336de7769e3b1f887d19a8a18f5b84d03
Tsunazuu <73156709+Tsunazuu@users.noreply.github.com>
2021-01-29 17:03:33 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ $(document).ready(function () {
   localStorage.getItem("fetchName");
   if (localStorage.getItem("fetchName") === "true") {
     var generateQn = Math.floor((Math.random() * 2) + 1);
-    if (generateQn === "1") {
-      fetchName()
-    } else {
-      fetchNames()
+    var triviaNumber = Math.floor((Math.random() * 2) + 1);
+    if (generateQn == "1" && triviaNumber == "1") {
+      fetchNameN()
     }
+    if (generateQn == "1" && triviaNumber == "2") {
+      fetchNameS()
+    }
+    if (generateQn == "2" && triviaNumber == "1") {
+      fetchShinyN()
+    }
+    if (generateQn == "2" && triviaNumber == "2") {
+      fetchShinyS()
+    }
+
     localStorage.removeItem("fetchName");
   }
 
 
-  function fetchName() {
+
+  function fetchNameN() {
 
     var pokemonNumber = Math.floor((Math.random() * 898) + 1);
-
-    var triviaNumber = Math.floor((Math.random() * 5) + 1);
 
     fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNumber + "/")
       .then(response => {
@@ -32,11 +40,22 @@ $(document).ready(function () {
         const html = [data]
           .map(pokemon => {
             return `
-          <div class="pokemon">
-
+            <div class="pokemon">
+    
             <img style ='image-rendering: pixelated;' src="pokemon/${pokemon.id}.png" class="card-img" alt="...">
-
+            <h1> Which Pokemon is This? </h1>
+            <form>
+            <div class="submitAnswer">
+              <div class="answerBar">
+               <input type="search" class="answer" id="answerBox" placeholder="Enter Answer">
+              </div>
+              <div class="answerBtn">
+                <button type="button" class="btn" id="answerButton"
+                onclick="script(getElementById('answerBox').value)">Enter</button>
+              </div>
             </div>
+
+          </div>
 
           `;
           })
@@ -49,11 +68,9 @@ $(document).ready(function () {
       })
   };
 
-  function fetchNames() {
+  function fetchNameS() {
 
     var pokemonNumber = Math.floor((Math.random() * 893) + 1);
-
-    var triviaNumber = Math.floor((Math.random() * 5) + 1);
 
     fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNumber + "/")
       .then(response => {
@@ -71,8 +88,19 @@ $(document).ready(function () {
               <div class="pokemon">
     
                 <img style ='image-rendering: pixelated;' src="pokemon/shiny/${pokemon.id}.png" class="card-img" alt="...">
-    
+                <h1> Which Pokemon is This? </h1>
+                <form>
+                <div class="submitAnswer">
+                  <div class="answerBar">
+                   <input type="search" class="answer" id="answerBox" placeholder="Enter Answer">
+                  </div>
+                  <div class="answerBtn">
+                    <button type="button" class="btn" id="answerButton"
+                    onclick="script(getElementById('answerBox').value)">Enter</button>
+                  </div>
                 </div>
+    
+              </div>
     
               `;
           })
@@ -86,6 +114,98 @@ $(document).ready(function () {
   };
 
 
+  function fetchShinyN() {
+
+    var pokemonNumber = Math.floor((Math.random() * 898) + 1);
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNumber + "/")
+      .then(response => {
+        if (!response.ok) {
+          throw Error("ERROR");
+        }
+        console.log(response.json)
+        return response.json();
+      })
+      .then(data => {
+        console.log(data)
+        const html = [data]
+          .map(pokemon => {
+            return `
+            <div class="pokemon">
+    
+            <img style ='image-rendering: pixelated;' src="pokemon/${pokemon.id}.png" class="card-img" alt="...">
+            <h1> Is This Pokemon Shiny? </h1>
+            <form>
+            <div class="submitAnswer">
+              <div class="answerBar">
+               <input type="search" class="answer" id="answerBox" placeholder="Enter Answer">
+              </div>
+              <div class="answerBtn">
+                <button type="button" class="btn" id="answerButton"
+                onclick="script(getElementById('answerBox').value)">Enter</button>
+              </div>
+            </div>
+
+          </div>
+
+          `;
+          })
+          .join("");
+        console.log(html)
+        document.querySelector("#app").insertAdjacentHTML("beforebegin", html);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
+
+
+  function fetchShinyS() {
+
+    var pokemonNumber = Math.floor((Math.random() * 893) + 1);
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNumber + "/")
+      .then(response => {
+        if (!response.ok) {
+          throw Error("ERROR");
+        }
+        console.log(response.json)
+        return response.json();
+      })
+      .then(data => {
+        console.log(data)
+        const html = [data]
+          .map(pokemon => {
+            return `
+              <div class="pokemon">
+    
+                <img style ='image-rendering: pixelated;' src="pokemon/shiny/${pokemon.id}.png" class="card-img" alt="...">
+                <h1> Is This Pokemon Shiny? </h1>
+                <form>
+                <div class="submitAnswer">
+                  <div class="answerBar">
+                   <input type="search" class="answer" id="answerBox" placeholder="Enter Answer">
+                  </div>
+                  <div class="answerBtn">
+                    <button type="button" class="btn" id="answerButton"
+                    onclick="script(getElementById('answerBox').value)">Enter</button>
+                  </div>
+                </div>
+    
+              </div>
+    
+              `;
+          })
+          .join("");
+        console.log(html)
+        document.querySelector("#app").insertAdjacentHTML("beforebegin", html);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
   $("#fetchName").on("click", function (e) {
     e.preventDefault();
     console.log('name clicked');
@@ -93,5 +213,6 @@ $(document).ready(function () {
     window.location.href = "trivia.html"
 
   })
+
 
 })
